fcbeda2fc99d022db3e102c8cbdcbfaffe56fba2
Thanapat1502 <psy150201027@gmail.com>
2024-10-24 13:42:54 +0700

    fix: waiting to fix

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+//ผู้ใช้สามารถกรอกฟอร์ม อัพเดทรายการ และลบรายการได้
+/**
+ * state ของ input
+ * state ของปุ่ม submit เป็นอาเรย์>>ฟังก์ชั่น add / ฟังก์ชั่น delete
+ * @returns
+ *   "Hello all ! This is first message.",
+ */
+const [inputText, setInputText] = useState("Hello all ! This is first message.");
+const [messageList, setMessageList] = useState([]);
+const updateText = (event) => {
+  //ใช้กับ onChange เก็บค่าไว้ที่ inputText
+  setInputText(event.target.value);
+};
+const handleAdd = () => {
+  //ก็อปอาเรย์ลงมาก่อนแล้วค่อยแก้ไขใน set แก้ค่า messageList
+  const newMessge = inputText;
+  setMessageList([...messageList, newMessge]);
+};
+const handleDelete = (index) => {
+  const updateList = messageList.filter((item) => {
+    item[index] !== index;
+  });
+  setMessageList(updateList);
+};
+
 function MessageBoard() {
   return (
     <div className="app-wrapper">
@@ -9,15 +35,23 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputText}
+            onChange={updateText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button " onClick={handleAdd}>
+          Submit
+        </button>
       </div>
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+        {messageList.map((item, index) => {
+          <div key={index} className="message">
+            <h1>{item}</h1>
+            <button className="delete-button" onClick={handleDelete}>
+              x
+            </button>
+          </div>;
+        })}
       </div>
     </div>
   );
